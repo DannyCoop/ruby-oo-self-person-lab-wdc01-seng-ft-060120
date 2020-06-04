b18b8cdeb84ab82189216738dd6e2a30b063c2d3
steve <stevepesce879@gmail.com>
2020-06-04 16:12:43 -0400

    Finished lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,9 @@
+require 'pry'
 class Person
-    attr_reader :name, :happiness, :hygiene
+    attr_reader \
+        :name,
+        :happiness,
+        :hygiene
     attr_accessor :bank_account
 
     def initialize(name)
@@ -10,38 +14,86 @@ class Person
     end
 
     def happiness=(happiness)
-        if happiness >= 10
-            @happiness = 10
-        elsif happiness < 0
-            @happiness = 0
-        else
-            @happiness = happiness
-        end
+        @happiness = bound_0_to_10(happiness)
     end
     
     def hygiene=(hygiene)
-        if hygiene >= 10
-            @hygiene = 10
-        elsif hygiene < 0
-            @hygiene = 0
+        @hygiene = bound_0_to_10(hygiene)
+    end
+
+    #helper: hygiene/happiness
+    #returns number but cant go outside of bounds (0..10)
+    def bound_0_to_10(num)
+        if num >= 10
+            return 10
+        elsif num < 0
+            return 0
         else
-            @hygiene = hygiene
+            return num
         end
     end
 
     def happy?
-        if @happiness > 7
-            return true
-        else
-            return false
-        end
+        @happiness > 7            
     end
 
     def clean?
-        if @hygiene > 7
-            return true
+        @hygiene > 7
+    end
+
+    # method should accept a salary amount and add this to the person's
+    #  bank account. Then the method should return 
+    #  the string "all about the benjamins".
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+    # The take_bath method should increment the person's hygiene points by four 
+    # and return the string "♪ Rub-a-dub just relaxing in the tub ♫".
+    def take_bath
+        self.hygiene=(@hygiene + 4)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"        
+    end
+    
+    # The work_out method should increment the person's happiness by two points, 
+    # decrease their hygiene by three points, and return the Queen lyrics, "♪ another 
+    # one bites the dust ♫".
+    def work_out
+        self.happiness  = (@happiness + 2) 
+        self.hygiene    = (@hygiene - 3)
+        return "♪ another one bites the dust ♫"
+    end
+    # he call_friend method should accept another instance of the Person class, or 
+    # "friend". The method should increment the caller and the callee's happiness 
+    # points by three. If Stella calls her friend Felix, the method should return 
+    # "Hi Felix! It's Stella. How are you?"
+    def call_friend(friend)        
+        self.happiness = (@happiness + 3)
+        friend.happiness = (friend.happiness + 3)
+        #binding.pry
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"        
+    end
+
+    # Finally, the start_conversation method should 
+    # accept two arguments, the person to start a 
+    # conversation with and the topic of conversation.
+    def start_conversation(person,topic)
+        # If the topic is politics, both people get 
+        # sadder and the method returns "blah blah partisan blah lobbyist".
+        if topic == "politics"
+            self.happiness = @happiness - 2
+            person.happiness = person.happiness - 2
+            return "blah blah partisan blah lobbyist"
+        # If the topic is weather, both people get a little happier 
+        # and the method returns "blah blah sun blah rain".
+        elsif topic == "weather"
+            self.happiness      = self.happiness + 1
+            person.happiness    = person.happiness + 1
+            return "blah blah sun blah rain"
+        # If the topic is not politics or weather, their happiness points 
+        # don't change and the method returns "blah blah blah blah blah".    
         else
-            return false
-        end
+            return "blah blah blah blah blah"
+        end        
     end
 end
